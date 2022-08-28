a18bda63b06896f72e510f0b0f2f9ea46f25484e
Luke <luciusfow@gmail.com>
2022-08-27 22:21:46 -0400

    cow by id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -6,8 +7,31 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  ## cows 
+
   get '/cows' do
     cows = Cow.all
     cows.to_json
   end
+
+  get '/cows/:id' do
+  cow = Cow.find(params[:id])
+  cow.to_json
+  end
+
+  ###delete cow
+
+
+
+  ##farm fields
+
+  get '/farm-fields' do
+    farm_fields = FarmField.all
+    farm_fields.to_json
+  end
+
+  get '/farm-field/:id' do
+    farm_field = FarmField.find(params[:id])
+    farm_field.to_json(include: :cows)
+    end
 end
